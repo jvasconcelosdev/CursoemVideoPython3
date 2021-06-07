d19d6ccf7de05a12d36bca921d54a3cebb605022
jvasconcelosdev <65770471+jvasconcelosdev@users.noreply.github.com>
2021-06-07 13:25:11 -0300

    Update exe32.py

--- exe32.py
+++ exe32.py
@@ -6,3 +6,5 @@ if ano % 4 == 0 and ano % 100 != 0 or ano  % 400 == 0:
     print('O ano {} e BISSEXTO'.format(ano))
 else:
     print('O ano {} nao e BISSEXTO'.format(ano))
+print('Fim do prgrama!!!')
+    
